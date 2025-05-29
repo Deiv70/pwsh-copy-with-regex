0a0371dac2d081ce6d7aad5e42ae0dbe4252d62f
David Gomez Garcia <david.gomez.garcia.000@gmail.com>
2025-05-29 16:01:02 +0200

    style: renaming

--- check_missing_media_files.sh
+++ check_missing_media_files.sh
@@ -6,8 +6,8 @@ DEST_PATH="$2"
 REGEXP_IGNORED_PATTERN=".*\.tmp$"
 REGEXP_PATTERN="\.(arw|cr2|cr3|nef|nrw|orf|raf|raw|rw2|dng|srw|pef|mos|kdc|3fr|x3f|mp4|mov|mkv|avi|wmv|flv|webm|m4v|mpg|mpeg|wav|aiff|aif|bwf|jpe?g|png|mp4)$"
 INDEX_FILENAME="index_mediaFiles.txt"
-MISSING_FILES_FILENAME="missing_mediaFiles_Name.txt"
-MISSING_FILEPATHS_FILENAME="missing_mediaFiles_NamePath.txt"
+MISSING_FILENAMES="missing_mediaFiles_Name.txt"
+MISSING_FILEPATHS="missing_mediaFiles_NamePath.txt"
 
 set -e
 #set -x
@@ -21,21 +21,21 @@ find "$DEST_PATH" -type f | sort -u | grep -viE "$REGEXP_IGNORED_PATTERN" 2>/dev
 echo "Generado Index en Destino: $DEST_PATH/$INDEX_FILENAME"
 
 comm -13 <(awk -F/ '{print $NF}' "$ORIGIN_PATH/$INDEX_FILENAME" | sort -u) <(awk -F/ '{print $NF}' "$DEST_PATH/$INDEX_FILENAME" | sort -u) \
-    > "$ORIGIN_PATH/$MISSING_FILES_FILENAME"
-echo "Generada lista de archivos faltantes en Origen: $ORIGIN_PATH/$MISSING_FILES_FILENAME"
+    > "$ORIGIN_PATH/$MISSING_FILENAMES"
+echo "Generada lista de archivos faltantes en Origen: $ORIGIN_PATH/$MISSING_FILENAMES"
 
 comm -23 <(awk -F/ '{print $NF}' "$ORIGIN_PATH/$INDEX_FILENAME" | sort -u) <(awk -F/ '{print $NF}' "$DEST_PATH/$INDEX_FILENAME"| sort -u) \
-    > "$DEST_PATH/$MISSING_FILES_FILENAME"
-echo "Generada lista de archivos faltantes en Destino: $DEST_PATH/$MISSING_FILES_FILENAME"
+    > "$DEST_PATH/$MISSING_FILENAMES"
+echo "Generada lista de archivos faltantes en Destino: $DEST_PATH/$MISSING_FILENAMES"
 
-if [ ! -s "$DEST_PATH/$MISSING_FILES_FILENAME" ]; then
+if [ ! -s "$DEST_PATH/$MISSING_FILENAMES" ]; then
     echo "No se han encontraron archivos faltantes en el Destino."
     exit 0
 fi
 
-[ -f "$DEST_PATH/$MISSING_FILEPATHS_FILENAME" ] && rm "$DEST_PATH/$MISSING_FILEPATHS_FILENAME"
-touch "$DEST_PATH/$MISSING_FILEPATHS_FILENAME"
+[ -f "$DEST_PATH/$MISSING_FILEPATHS" ] && rm "$DEST_PATH/$MISSING_FILEPATHS"
+touch "$DEST_PATH/$MISSING_FILEPATHS"
 while read -r fileName; do
     echo "$fileName :"
-    awk -F/ -v fileName="$fileName" '$NF == fileName { print }' "$ORIGIN_PATH/$INDEX_FILENAME" | tee -a "$DEST_PATH/$MISSING_FILEPATHS_FILENAME"
-done < "$DEST_PATH/$MISSING_FILES_FILENAME"
+    awk -F/ -v fileName="$fileName" '$NF == fileName { print }' "$ORIGIN_PATH/$INDEX_FILENAME" | tee -a "$DEST_PATH/$MISSING_FILEPATHS"
+done < "$DEST_PATH/$MISSING_FILENAMES"
